a19c7d3b7521421954deada03546c15a47587193
Edmund Home <edmundhome@hotmail.com>
2019-09-04 13:41:54 +0200

    Actualizacion angular 2

--- lucatinder-cliente/src/app/app-routing.module.ts
+++ lucatinder-cliente/src/app/app-routing.module.ts
@@ -3,6 +3,11 @@ import { Routes, RouterModule } from '@angular/router';
 import { ListadoComponent } from './componentes/listado/listado.component';
 import { InicioComponent } from './componentes/inicio/inicio.component';
 import { ContactosComponent } from './componentes/contactos/contactos.component';
+import { DescartesComponent } from './componentes/descartes/descartes.component';
+import { MatchesComponent } from './componentes/matches/matches.component';
+import { ChatComponent } from './componentes/chat/chat.component';
+import { PerfilComponent } from './componentes/perfil/perfil.component';
+import { SelectorComponent } from './componentes/selector/selector.component';
 
 const routes: Routes = [
   {
@@ -22,6 +27,28 @@ const routes: Routes = [
   {
     path: 'contactos',
     component: ContactosComponent
+  }
+  ,
+  {
+    path: 'descartes',
+    component: DescartesComponent
+  }
+  ,
+  {
+    path: 'selector',
+    component: SelectorComponent
+  },
+  {
+    path: 'matches',
+    component: MatchesComponent
+  },
+  {
+    path: 'chat',
+    component: ChatComponent
+  },
+  {
+    path: 'perfil',
+    component: PerfilComponent
   }
   
 ];
